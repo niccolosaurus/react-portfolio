bcf04eda21002e4ab8160467e792e569f8f50730
Niccolo Eck <nicco.eck@gmail.com>
2022-05-05 23:24:53 -0700

    changed code to reflect new file of Header.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <Navbar buttonText={'button'}/>
-
+      <Header buttonText={'button'}/>
     </div>
   );
 }
