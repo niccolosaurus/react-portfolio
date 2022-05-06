62c6687898957127b738c668fd6898341e01b34a
Niccolo Eck <nicco.eck@gmail.com>
2022-05-05 23:54:16 -0700

    Added code so my components will be rendered

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <Header buttonText={'button'}/>
+    <div>
+      <div className="App">
+        <Header buttonText={'button'}/>
+      </div>
+
+      <div>
+        <Main />
+      </div>
+
+      <div>
+        <Footer />
+      </div>
     </div>
   );
 }
