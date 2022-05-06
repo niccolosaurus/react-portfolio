35e6fa59ff2387f959a239c82b7b4420ecc5522f
Niccolo Eck <nicco.eck@gmail.com>
2022-05-05 23:46:30 -0700

    added my code for my Footer.js file. Contains my contact information

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer id="contact-me">
+          <h2>Contact me</h2>
+          <p><a href="mailto:nicco.eck@gmail.com">nicco.eck@gmail.com</a></p>
+          <p>619-219-9214</p>
+          <p><a href="./src/images/Niccolo-Eck-Resume.pdf" download="Niccolo-Eck-Resume">Resume</a></p>
+      </footer>
+    );
+};
+
+export default Footer;
