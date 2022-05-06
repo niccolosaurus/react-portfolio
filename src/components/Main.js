32877f6e391db4adaccc7391a9a155c09078e356
Niccolo Eck <nicco.eck@gmail.com>
2022-05-05 23:48:03 -0700

    Added code to my Main.js. This is the main body of my code

---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Main() {
+    return (
+        <section class="sub-section" id="about-me">
+        <div class="information">
+            <h2>About Me</h2>
+            <p>My name is Niccolo Eck, and I am currently going through a full stack boot camp from UCSD. So far I have a knowledge of HTML, CSS, JS, Node.js and OOP.</p>
+        </div>
+        <div class="photo-container">
+            <img class="photo" src="./src/images/professional-photo.jpg" alt="Niccolo Eck photo"/>
+        </div>
+    </section>
+    )
+};
+
+export default Main;
