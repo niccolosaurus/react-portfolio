61f93ef226a965ee43315a52e99bf612b5edbe2c
Niccolo Eck <nicco.eck@gmail.com>
2022-05-06 17:53:25 -0700

    created a loop of data for my ProjectCard.js file

---
+++ src/data.js
@@ -0,0 +1,55 @@
+import nationalParkImg from "./images/national-park-project.PNG";
+import slitherInImg from "./images/slither-in-project.PNG";
+import weatherImg from "./images/weather-dashboard.PNG";
+import workdayImg from "./images/work-day-scheduler.PNG";
+
+export const projectData = [
+  {
+    title: "National Parks Search Project",
+    imgSrc: nationalParkImg,
+    alt: "National Parks Search Project",
+    description:
+      "This project was designed to find national parks in the United States and its territories and get information about them.",
+    type: "Front End Project",
+    languages: "Languages: HTML, CSS, JS",
+    tech: "Tech: Modals, API's, Bootstrap",
+    site: "https://niccolosaurus.github.io/National-Park-Project/",
+    repo: "https://github.com/niccolosaurus/National-Park-Project",
+  },
+  {
+    title: "Slither-In Website Project",
+    imgSrc: slitherInImg,
+    alt: "Slither-In Website Project",
+    description:
+      "For this project we created a website that allows you to keep tract of your reptiles, complete with images and in depth descriptions.",
+    type: "Full Stack Project<",
+    languages: "Languages: HTML, CSS, JS",
+    tech: "Tech: MySQL, MVC, NPM(packages), Firebase, DropZone",
+    site: "https://slither-in.herokuapp.com/",
+    repo: "https://github.com/niccolosaurus/slither-in",
+  },
+  {
+    title: "Weather Dashboard",
+    imgSrc: weatherImg,
+    alt: "Weather Dashboard Project",
+    description:
+      "Want to know the weather of a city, look no further than this weather dashboard I designed. It will also generate a 5-day forcast for your future planning.",
+    type: "Front End Project",
+    languages: "Languages: HTML, CSS, JS",
+    tech: "Tech: API, Local Storage",
+    site: "https://niccolosaurus.github.io/weather-dashboard/",
+    repo: "https://github.com/niccolosaurus/weather-dashboard",
+  },
+  {
+    title: "Workday Scheduler",
+    imgSrc: workdayImg,
+    alt: "Workday Scheduler Project",
+    description:
+      "Keep track of your busy work day and your upcoming meetings. As the day progresses the color will change with the past, present, and future.",
+    type: "Front End Project",
+    languages: "Languages: HTML, CSS, JS",
+    tech: "Tech: Moment",
+    site: "https://niccolosaurus.github.io/work-day-schedule/",
+    repo: "https://github.com/niccolosaurus/work-day-schedule",
+  },
+];
