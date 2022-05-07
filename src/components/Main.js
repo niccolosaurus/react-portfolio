56595f4230b339fa6321116b3376fe5074ad8f07
Niccolo Eck <nicco.eck@gmail.com>
2022-05-06 17:28:49 -0700

    Updated my Main with a more accurate About Me section and my photo now displays.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,21 @@
 import React from "react";
+import profilePic from "../images/professional-photo.jpg";
 
 function Main() {
-    return (
-        <section class="sub-section" id="about-me">
-        <div class="information">
-            <h2>About Me</h2>
-            <p>My name is Niccolo Eck, and I am currently going through a full stack boot camp from UCSD. So far I have a knowledge of HTML, CSS, JS, Node.js and OOP.</p>
-        </div>
-        <div class="photo-container">
-            <img class="photo" src="./src/images/professional-photo.jpg" alt="Niccolo Eck photo"/>
-        </div>
+  return (
+    <section className="sub-section" id="about-me">
+      <div className="information">
+        <h2>About Me</h2>
+        <p>
+          My name is Niccolo Eck, and I am currently going through a full stack
+          boot camp from UCSD. I am able to work with and use HTML, CSS JS, and React.js on the front end, and for the backend I know Node.js, APIs, Express, SQL, and MongoDB.  
+        </p>
+      </div>
+      <div className="photo-container">
+        <img className="photo" src={profilePic} alt="profile of nicco"/>
+      </div>
     </section>
-    )
-};
+  );
+}
 
 export default Main;
