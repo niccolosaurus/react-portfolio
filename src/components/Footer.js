311aeb13123833ae82498e43f74b656d50774d31
Niccolo Eck <nicco.eck@gmail.com>
2022-05-06 17:26:45 -0700

    Footer compelted, and now you can dowload a pdf of my resume

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,21 @@
 import React from "react";
+import resume from "../images/Niccolo-Eck-Resume.pdf";
 
 function Footer() {
-    return (
-        <footer id="contact-me">
-          <h2>Contact me</h2>
-          <p><a href="mailto:nicco.eck@gmail.com">nicco.eck@gmail.com</a></p>
-          <p>619-219-9214</p>
-          <p><a href="./src/images/Niccolo-Eck-Resume.pdf" download="Niccolo-Eck-Resume">Resume</a></p>
-      </footer>
-    );
-};
+  return (
+    <footer id="contact-me">
+      <h2>Contact me</h2>
+      <p>
+        <a href="mailto:nicco.eck@gmail.com">nicco.eck@gmail.com</a>
+      </p>
+      <p>619-219-9214</p>
+      <p>
+        <a href={resume} download="Niccolo-Eck-Resume">
+          Resume
+        </a>
+      </p>
+    </footer>
+  );
+}
 
 export default Footer;
